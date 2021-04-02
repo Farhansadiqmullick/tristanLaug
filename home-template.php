271c57a82d56b15de3e29f4e5a95999a90b872f7
Farhan <farhansadiq24@gmail.com>
2021-04-02 19:38:01 +0600

    2nd feb file added

--- home-template.php
+++ home-template.php
@@ -46,9 +46,10 @@ $description = $banner['description'];
 							<?php
 								$posts = $films['film'];
 								if(have_posts($posts)):	
+									
 								
 							?>
-							<a href="<?php the_permalink(); ?>" class="films__card has--overlay">
+							<a href="<?php the_permalink(150) ;?>" class="films__card has--overlay">
 								<figure class="films__card-media">
 									<?php
 									$image = get_the_post_thumbnail_url( $posts->ID, 'large');
@@ -63,43 +64,37 @@ $description = $banner['description'];
 								<div class="films__card-text text-center">
 									<h2 class="title text-uppercase"><?php echo esc_html($posts->post_title); ?></h2>
 									<?php $logos = $films['logos'];
-									
-									$logo1= $logos[0]; 
-									$logo2= $logos[1]; 
-									$logo3= $logos[2]; 
-									$logo4= $logos[3]; 
 									?>
 									<div class="festival-logos">
 										<figure class="media">
-												<?php if($logo1){
-													echo wp_get_attachment_image($logo1, array(263,132));
+												<?php if($logos[0]){
+													echo wp_get_attachment_image($logos[0], array(263,132));
 												}
 												?>
 										</figure>
 										<figure class="media">
-										<?php if($logo2){
-											echo wp_get_attachment_image($logo2, array(263,132));
-										}
-										?>
+											<?php if($logos[1]){
+												echo wp_get_attachment_image($logos[1], array(263,132));
+												}
+											?>
 										</figure>
 										<figure class="media">
-										<?php if($logo3){
-											echo wp_get_attachment_image($logo3, array(263,132));
-										}
-										?>
+											<?php if($logos[2]){
+												echo wp_get_attachment_image($logos[2], array(263,132));
+												}
+											?>
 										</figure>
 										<figure class="media">
-										<?php if($logo3){
-											echo wp_get_attachment_image($logo4, array(263,132));
-										}
-										?>
+											<?php if($logos[3]){
+												echo wp_get_attachment_image($logos[3], array(263,132));
+												}
+											?>
 										</figure>
 									</div>
 								</div>
 							</a>
 
-							<?php 
-							endif;?>
+							<?php endif;?>
 						</div><!-- /films__card -->
 
 						<div class="col-12">
@@ -144,7 +139,7 @@ $description = $banner['description'];
 						</div>
 					</div>
 							<?php $galleries = $designs['gallery']; 
-							var_dump($galleries);
+							
 							// if(have_posts($galleries)) :
 							// 	foreach($galleries as $gallery) :
 							// 		var_dump($gallery);
@@ -167,7 +162,7 @@ $description = $banner['description'];
 			<section class="hm-about-us" id="scroll-contact">
 				<div class="container">
 					<div class="row">
-                        <?php echo get_field('about'); 
+						<?php $about = get_field('about'); 
                        ?>
 						<div class="col-12">
 							<div class="hm-about-us__content text-center">
